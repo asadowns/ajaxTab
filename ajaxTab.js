899c9a4ef5243e7858a92edd611324fb59707ed7
Asa Downs <asadowns@gmail.com>
2013-10-03 17:17:42 -0700

    Refactored load statement and adjusted result handling

--- ajaxTab.js
+++ ajaxTab.js
@@ -1,54 +1,63 @@
 var AsaCode  = {};
 
 AsaCode.TabInterface = Class.extend({
-		
-	init: function(el) {
-	  this.getTarget(el);
-	},
-	getTarget : function(el) {
-		parentEl = $(el).parents("ul");
-		currentTarget = parentEl.data("config");
-		updateTarget = $("div").filter(function(){
-			possibles = $(this).data("target-id");
-			return possibles.ajaxTarget === currentTarget.ajaxTarget;
-		});
-		this.loadAjaxRequest(el,updateTarget);
-	},
-	loadAjaxRequest : function(el,updateTarget) {
-		updateTarget.load(el.href);
-	}
+    init: function(el) {
+        this.el = el;
+        this.handleTabClick();
+    },
 
+    handleTabClick : function() {
+        boundLoadResults= $.proxy(this.loadResults, this);
+        $(this.el).on('click', 'a', boundLoadResults);
+    },
+
+    loadResults : function (event) {
+        this.getTarget();
+        this.loadAjaxRequest(event);
+    },
+
+    getTarget : function() {
+        currentTarget = $(this.el).data("config");
+        this.updateTarget = $("div").filter(function(){
+            possibles = $(this).data("target-id");
+            return possibles.ajaxTarget === currentTarget.ajaxTarget;
+        });
+    },
+
+    loadAjaxRequest : function(event) {
+        event.preventDefault();
+        if (event.target.href) {
+            this.updateTarget.load(event.target.href, function(){
+                AsaCode.Tabs.init();
+        });
+    }
+}
 });
 
 AsaCode.Tabs = AsaCode.Tabs || {
 
-	tabClass : 'ajaxClickable',
-	
-	selector : '.',
-	
-	Objects : [],
-	
-	init : function() {
-		this.selector += this.tabClass;
-		boundAttachEvent = $.proxy(this.attachEvent, AsaCode.Tabs)
-		$(this.selector).on('click', 'a', boundAttachEvent);
-	},
-	attachEvent : function(event) {
-		event.preventDefault();
-		el = event.target;
-		this.create(el);
-		this.markInitialized(el);
-	},
-	
+    tabClass : 'ajaxClickable',
+
+    Objects : [],
+
+    init : function() {
+        boundInitializeTabInterface = $.proxy(this.initializeTabInterface, AsaCode.Tabs);
+        $('.' + this.tabClass).each(boundInitializeTabInterface);
+    },
+    initializeTabInterface : function(index, el) {
+        this.create(el);
+        this.markInitialized(el);
+    },
+
     create : function(el) {
-		Objects = this.Objects;
+        Objects = this.Objects;
         (Objects).push(new AsaCode.TabInterface(el));
     },
-	markInitialized: function(el){
-		parentEl = $(el).parents("ul");
+    markInitialized: function(el){
+        parentEl = $(el);
         if (parentEl.hasClass(this.tabClass)){
-          parentEl.removeClass(this.tabClass);
-          parentEl.addClass(this.tabClass+'-initialized');
+            parentEl.removeClass(this.tabClass);
+            parentEl.addClass(this.tabClass+'-initialized');
         }
     }
 
@@ -56,3 +65,4 @@ AsaCode.Tabs = AsaCode.Tabs || {
 
 var TabsInit = AsaCode.Tabs.init();
 $(document).ready(TabsInit);
+
